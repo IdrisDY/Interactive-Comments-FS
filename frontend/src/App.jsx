655c45fe8a339d4e58513a6746e09a46b3e23160
DisuAde <idrisadekunle726@gmail.com>
2024-05-21 08:29:20 +0100

    add readme to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,22 @@ import Post_Comment from './Comments/Post-Comment';
 import Reducer from './Comments/reply';
 
 function App() {
+
+useEffect( ()=>{
+  async function call (){
+    try {
+      const response = await fetch('/api/comments',{method:'GET'})
+      const json = await response.json()
+      console.log(json,"Oh my sins")
+  
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  call()
+},[])
+
+
   return (
     <div className="App">
      <Comment/>
--- frontend/src/context/comments.jsx
+++ frontend/src/context/comments.jsx
@@ -1,10 +1,35 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 export const CommentContext =  createContext({msg:'reason'})
+export const commentReducer = (state,action)=>{
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+        return{
+            commentInformation:action.payload
+        }
 
+    case 'CREATE_WORKOUTS':
+        return{
+            commentInformation:[action.payload ,...state.workouts]
+        }
+    case 'DELETE_WORKOUT':
+
+        return{
+            commentInformation:state.workouts.filter(item=>item._id !== action.payload._id)
+        }
+    default:
+        return state
+        break;
+}
+
+}
 export function CommentContextProvider({children}){
 
-  return <CommentContext.Provider value={'Moogli'}>
+  const [state,dispatch] = useReducer(commentReducer,{
+    commentsInformation:null
+  })
+
+  return <CommentContext.Provider value={{...state,dispatch}}>
     {children}
   </CommentContext.Provider>
 }
