b9b8dba00e558b54a986e241fff675e6da79858e
DisuAde <idrisadekunle726@gmail.com>
2024-05-28 02:51:29 +0100

    changed proxy to render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://interactive-comments-backend-1.onrender.com/",
         changeOrigin: true,
         secure: false,
       },
