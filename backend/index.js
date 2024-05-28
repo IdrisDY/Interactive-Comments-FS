d6a2a2cb217a4ad33e5da91caff4f2537b018c8a
DisuAde <idrisadekunle726@gmail.com>
2024-05-28 03:00:56 +0100

    allow cors

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const routes = require("./routes/comments");
 require('dotenv').config()
 
 app.use(express.json())
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 mongoose
   .connect(process.env.MONGO_URL)
   .then((res) => { 
